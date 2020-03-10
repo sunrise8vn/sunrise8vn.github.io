e45af5c56f052f38bddf3f31a069750bc31360eb
Minh Bui <sunrsie8vn@gmail.com>
2020-03-10 11:06:38 +0700

    .....

---
+++ cafe/js/custom.script.js
@@ -0,0 +1,110 @@
+$( document ).ready(function() {
+    $(".num-table a").on("click", function() {
+    	$(".num-table").removeClass("active");
+    	$(this).parent().addClass("active");
+    	$(".main-order").css("display", "none");
+    	$($(this).attr("href")).css("display", "block");
+    	// alert("table click");
+    	return false;
+    });
+
+    $(".bill-items").draggable();
+    // bill-items
+
+    // $(".order-item a").mouseover(function() {
+    // 	// $(".order-item a .item-img").css("display", "none");
+    // 	$(this).children().css("display", "none");
+    // 	// $(this).parent().(".item-btn").css("display", "block");
+    // 	$($(this).attr("href")).css("display", "block");
+    	
+    // });
+
+    // $(".order-item a").mouseout(function() {
+    // 	$(".order-item a .item-img").css("display", "block");
+    // 	// $(this).parent().(".item-btn").css("display", "block");
+    // 	$($(this).attr("href")).css("display", "none");
+    // });
+
+    // $(".order-item").mouseover(function() {
+    // 	$(this).css("display", "none");
+    // 	$(".item-btn").css("display", "block");
+    // 	$("p").css("color", "#fff");
+    // 	$(this).css("background-color", "#fba154;");
+    // });
+
+    // $(".order-item").mouseout(function() {
+    // 	$(".item-img").css("display", "block");
+    // 	$(".item-btn").css("display", "none");
+    // 	// $(this).css("display", "none");
+    // 	// $("p").css("color", "#000");
+    // 	// $(this).css("background-color", "#fff;");
+    // });
+
+    $(".order-items .item-btn .btn").on("click", function() {
+    	// alert("item click");
+    	let itemId = $(this).data("id");
+    	let name = $(this).data("name");
+    	let price = $(this).data("price");
+
+		let k = false;
+		$.each(table01, function() {
+		    if (this.item_id == itemId) {
+		    	let q = this.item_quantity + 1;
+		        this.item_quantity = q;
+		        this.money = q * price;
+		        k = true;
+		    }
+		});
+
+		if(!k) {
+			table01.push({
+              	"id": bill_detail_id_max + 1,
+				"bill_id": 1,
+				"table_id": 1,
+				"item_id": itemId,
+				"item_name": name,
+				"item_price": price,
+				"item_quantity": 1,
+				"money": price
+            });
+            
+	    	let str = '' +
+		    	'<div class="bill-items">' +
+			    '<div class="col-md-7">'+
+			    '<p>'+name+'</p>'+
+			    '<p>Giá: '+addCommas(price)+' vnđ</p>'+
+			    '</div>'+
+			    '<div class="col-md-1" style="text-align: center;">'+
+			    '<p>&nbsp;</p>'+
+			    '<p>1</p>'+
+			    '</div>'+
+			    '<div class="col-md-4" style="text-align: right;">'+
+			    '<p>&nbsp;</p>'+
+			    '<p>'+addCommas(price)+' vnđ</p>'+
+			    '</div>'+
+			    '</div>';
+	    	$("#table-bill-1").append(str);
+		}
+		else {
+			let str = '';
+			for (var i = 0; i < table01.length; i++) {
+			    str += '<div class="bill-items">' +
+				    '<div class="col-md-7">'+
+				    '<p>'+table01[i].item_name+'</p>'+
+				    '<p>Giá: '+addCommas(table01[i].item_price)+' vnđ</p>'+
+				    '</div>'+
+				    '<div class="col-md-1" style="text-align: center;">'+
+				    '<p>&nbsp;</p>'+
+				    '<p>'+table01[i].item_quantity+'</p>'+
+				    '</div>'+
+				    '<div class="col-md-4" style="text-align: right;">'+
+				    '<p>&nbsp;</p>'+
+				    '<p>'+addCommas(table01[i].money)+' vnđ</p>'+
+				    '</div>'+
+				    '</div>';
+			}
+			$("#table-bill-1").html(str);
+		}
+    });
+
+});
---
+++ cafe/js/script.js
@@ -0,0 +1,8 @@
+function addCommas(nStr) {
+    nStr += '';
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(nStr)) {
+        nStr = nStr.replace(rgx, '$1' + '.' + '$2');
+    }
+    return nStr;
+}
