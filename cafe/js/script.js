329c18dffd5fa461c8a73a6b6f1b9e0c145a2a56
Minh Bui <sunrsie8vn@gmail.com>
2020-03-10 14:19:53 +0700

    .....

--- cafe/js/script.js
+++
@@ -1,8 +0,0 @@
-function addCommas(nStr) {
-    nStr += '';
-    var rgx = /(\d+)(\d{3})/;
-    while (rgx.test(nStr)) {
-        nStr = nStr.replace(rgx, '$1' + '.' + '$2');
-    }
-    return nStr;
-}
